7974485686288696d6a4b86e0b49a0194ba16f09
May Ng <mngmay@gmail.com>
2019-07-19 11:06:45 -0700

    Added ButtonsDiv styled component. Working on btn styling

--- starwars/src/components/Buttons.js
+++ starwars/src/components/Buttons.js
@@ -1,13 +1,25 @@
 import React from "react";
+import styled from "styled-components";
+
+const ButtonsDiv = styled.div`
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+  width: 30%;
+`;
 
 const Buttons = props => {
   const { nextPage, prevPage } = props;
 
   return (
-    <div>
-      <button onClick={prevPage}>Previous Page</button>
-      <button onClick={nextPage}>Next Page</button>
-    </div>
+    <ButtonsDiv>
+      <button onClick={prevPage} type="prev">
+        Previous Page
+      </button>
+      <button onClick={nextPage} type="next">
+        Next Page
+      </button>
+    </ButtonsDiv>
   );
 };
 export default Buttons;
